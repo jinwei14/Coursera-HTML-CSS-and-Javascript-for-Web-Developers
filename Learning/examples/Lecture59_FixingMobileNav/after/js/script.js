ce5181031c9dcb052a766f967c2f21bff05e87c0
jinwei14 <janviezhang14@gmail.com>
2019-05-03 14:21:12 +0100

    adding more comments to mobile nav bar tab to other place and collapse the nav bar

--- Learning/examples/Lecture59_FixingMobileNav/after/js/script.js
+++ Learning/examples/Lecture59_FixingMobileNav/after/js/script.js
@@ -3,6 +3,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
+    // if we are in the mobile view
     if (screenWidth < 768) {
       $("#collapsable-nav").collapse('hide');
     }
